f9e9380c99f79178743d59961aa1018feaeedd8e
putra-inttech <putraevans001@gmail.com>
2020-06-20 14:20:18 +0700

    2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,16 +10,16 @@
   <!-- Bootstrap 3.3.7 -->
 <link rel="stylesheet" href="{{asset('/assets/bower_components/bootstrap/dist/css/bootstrap.min.css')}}">
   <!-- Font Awesome -->
-<link rel="stylesheet" href="{{('/assets/bower_components/font-awesome/css/font-awesome.min.css')}}">
+<link rel="stylesheet" href="{{asset('/assets/bower_components/font-awesome/css/font-awesome.min.css')}}">
   <!-- Ionicons -->
-<link rel="stylesheet" href="{{('/assets/bower_components/Ionicons/css/ionicons.min.css')}}">
+<link rel="stylesheet" href="{{asset('/assets/bower_components/Ionicons/css/ionicons.min.css')}}">
   <!-- DataTables -->
-<link rel="stylesheet" href="{{('/assets/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{asset('/assets/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css')}}">
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{('/assets/dist/css/AdminLTE.min.css')}}">
+  <link rel="stylesheet" href="{{asset('/assets/dist/css/AdminLTE.min.css')}}">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
-  <link rel="stylesheet" href="{{('/assets/dist/css/skins/_all-skins.min.css')}}">
+  <link rel="stylesheet" href="{{asset('/assets/dist/css/skins/_all-skins.min.css')}}">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -339,20 +339,20 @@
 <!-- ./wrapper -->
 
 <!-- jQuery 3 -->
-<script src="{{('/assets/bower_components/jquery/dist/jquery.min.js')}}"></script>
+<script src="{{asset('/assets/bower_components/jquery/dist/jquery.min.js')}}"></script>
 <!-- Bootstrap 3.3.7 -->
-<script src="{{('/assets/bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('/assets/bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
 <!-- DataTables -->
-<script src="{{('/assets/bower_components/datatables.net/js/jquery.dataTables.min.js')}}"></script>
-<script src="{{('/assets/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js')}}"></script>
+<script src="{{asset('/assets/bower_components/datatables.net/js/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('/assets/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js')}}"></script>
 <!-- SlimScroll -->
-<script src="{{('/assets/bower_components/jquery-slimscroll/jquery.slimscroll.min.js')}}"></script>
+<script src="{{asset('/assets/bower_components/jquery-slimscroll/jquery.slimscroll.min.js')}}"></script>
 <!-- FastClick -->
-<script src="{{('/assets/bower_components/fastclick/lib/fastclick.js')}}"></script>
+<script src="{{asset('/assets/bower_components/fastclick/lib/fastclick.js')}}"></script>
 <!-- AdminLTE App -->
-<script src="{{('/assets/dist/js/adminlte.min.js')}}"></script>
+<script src="{{asset('/assets/dist/js/adminlte.min.js')}}"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="{{('/assets/dist/js/demo.js')}}"></script>
+<script src="{{asset('/assets/dist/js/demo.js')}}"></script>
 <script>
   $(document).ready(function () {
     $('.sidebar-menu').tree()
